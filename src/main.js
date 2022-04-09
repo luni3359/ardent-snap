018f52697be040997bcda362a0367fdf4ae78f37
luni3359 <lunibackup@outlook.com>
2022-04-09 01:03:31 -0500

    Create Vector2D

--- src/main.js
+++ src/main.js
@@ -40,55 +40,69 @@ GameCanvas.prototype.setResolution = function (w, h) {
     this._canvas.height = this._resolution[1];
 };
 
+
+function Vector2D(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+}
+
+Vector2D.prototype.magnitude = function () {
+    return Math.sqrt(Math.abs(Math.pow(this.x, 2) + Math.pow(this.y, 2)));
+};
+
+Vector2D.prototype.unit = function () {
+    const magnitude = this.magnitude;
+    return new Vector2D(this.x / magnitude, this.y / magnitude);
+};
+
 function Player() {
     this.size = 50;
-    this.x = 0;
-    this.y = 0;
+    this.position = new Vector2D();
     this.speed = 1;
 }
 
 Player.prototype.update = function (dt) {
-    let direction = [0, 0];
+    let direction = new Vector2D();
 
     if (keys["a"]) {
-        direction[0] -= 1;
+        direction.x -= 1;
     }
     if (keys["d"]) {
-        direction[0] += 1;
+        direction.x += 1;
     }
     if (keys["w"]) {
-        direction[1] -= 1;
+        direction.y -= 1;
     }
     if (keys["s"]) {
-        direction[1] += 1;
+        direction.y += 1;
     }
 
-    if (direction[0] && direction[1]) {
-        let magnitude = Math.sqrt(Math.pow(direction[0], 2), Math.pow(direction[1], 2));
-        this.x += direction[0] / magnitude;
-        this.y += direction[1] / magnitude;
+    if (direction.x && direction.y) {
+        let magnitude = Math.sqrt(Math.pow(direction.x, 2), Math.pow(direction.y, 2));
+        this.position.x += direction.x / magnitude;
+        this.position.y += direction.y / magnitude;
     } else {
-        this.x += direction[0];
-        this.y += direction[1];
+        this.position.x += direction.x;
+        this.position.y += direction.y;
     }
 
-    print(`x: ${this.x}, y: ${this.y}`);
+    print(`x: ${this.position.x}, y: ${this.position.y}`);
 
-    if (this.x + this.size > canvas._resolution[0]) {
-        this.x = canvas._resolution[0] - this.size;
-    } else if (this.x < 0) {
-        this.x = 0;
+    if (this.position.x + this.size > canvas._resolution[0]) {
+        this.position.x = canvas._resolution[0] - this.size;
+    } else if (this.position.x < 0) {
+        this.position.x = 0;
     }
 
-    if (this.y + this.size > canvas._resolution[1]) {
-        this.y = canvas._resolution[1] - this.size;
-    } else if (this.y < 0) {
-        this.y = 0;
+    if (this.position.y + this.size > canvas._resolution[1]) {
+        this.position.y = canvas._resolution[1] - this.size;
+    } else if (this.position.y < 0) {
+        this.position.y = 0;
     }
 };
 
 Player.prototype.draw = function () {
-    ctx.fillRect(this.x, this.y, this.size, this.size);
+    ctx.fillRect(this.position.x, this.position.y, this.size, this.size);
 };
 
 let players = [];
