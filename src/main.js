207ee0396ec5809f48e1ad3472035a7fdf465cdf
luni3359 <lunibackup@outlook.com>
2022-04-08 23:52:55 -0500

    Separate player into its own class

--- src/main.js
+++ src/main.js
@@ -38,47 +38,66 @@ GameCanvas.prototype.setResolution = function (w, h) {
     this._canvas.height = this._resolution[1];
 };
 
-let x = 0;
-let y = 0;
+function Player() {
+    this.size = 50;
+    this.x = 0;
+    this.y = 0;
+}
+
+Player.prototype.update = function (dt) {
+    if (keys["a"]) {
+        this.x -= 1 * dt;
+    }
+    if (keys["d"]) {
+        this.x += 1 * dt;
+    }
+    if (keys["w"]) {
+        this.y -= 1 * dt;
+    }
+    if (keys["s"]) {
+        this.y += 1 * dt;
+    }
+
+    if (this.x + this.size > canvas._resolution[0]) {
+        this.x = canvas._resolution[0] - this.size;
+    } else if (this.x < 0) {
+        this.x = 0;
+    }
+
+    if (this.y + this.size > canvas._resolution[1]) {
+        this.y = canvas._resolution[1] - this.size;
+    } else if (this.y < 0) {
+        this.y = 0;
+    }
+};
+
+Player.prototype.draw = function () {
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+};
+
+let players = [];
 let update_last = performance.now();
 function update() {
     let update_now = performance.now();
     const dt = update_now - update_last;
     // print(dt);
     update_last = update_now;
-    
-    if (keys["a"]) {
-        x -= 1 * dt;
-    }
-    if (keys["d"]) {
-        x += 1 * dt;
-    }
-    if (keys["w"]) {
-        y -= 1 * dt;
-    }
-    if (keys["s"]) {
-        y += 1 * dt;
-    }
 
-    if (x + 50 > canvas._resolution[0]) {
-        x = canvas._resolution[0] - 50;
-    } else if (x  < 0) {
-        x = 0;
-    }
-
-    if (y + 50 > canvas._resolution[1]) {
-        y = canvas._resolution[1] - 50;
-    } else if (y  < 0) {
-        y = 0;
+    for (let i = 0; i < players.length; i++) {
+        players[i].update(dt);
     }
 }
 
 draw_last = performance.now();
 function draw(ct) {
-    print(ct - draw_last)
+    // print(ct - draw_last)
     draw_last = ct;
     ctx.clearRect(0, 0, 640, 720);
-    ctx.fillRect(x, y, 50, 50);
+
+    for (let i = 0; i < players.length; i++) {
+        players[i].draw();
+    }
+
     requestAnimationFrame(draw)
 }
 
@@ -90,6 +109,7 @@ function gameRun() {
 function main() {
     canvas = new GameCanvas(document.getElementById("game-window"));
     ctx = canvas._canvas.getContext("2d");
+    players.push(new Player());
     gameRun();
 }
 
