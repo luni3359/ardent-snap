22b7638f3a8bfd2829aa658661fef67fd4a916e4
luni3359 <lunibackup@outlook.com>
2022-05-05 02:16:30 -0500

    Change asset loading, for images only

--- src/fetch.js
+++ src/fetch.js
@@ -1,22 +1,18 @@
-import characters from "../static/sprites/characters.png";
-import fonts from "../static/sprites/fonts.png";
-import hud from "../static/sprites/hud.png";
-import menus from "../static/sprites/menus.png";
-import projectiles from "../static/sprites/projectiles.png";
 import data from "../static/sprites/spriteInfo.json";
 import { AssetManager } from "./ardent/assets";
+// import menuMusic from "../static/music/01. Sealed Gods.mp3";
 
 
 export async function loadAssets() {
     const essentialAssets = [
-        { name: "menus", src: menus },
         { name: "data", src: data },
+        { name: "menus", src: new URL("../static/sprites/menus.png", import.meta.url) },
     ];
     const assets = [
-        { name: "hud", src: hud },
-        { name: "fonts", src: fonts },
-        { name: "characters", src: characters },
-        { name: "projectiles", src: projectiles },
+        { name: "hud", src: new URL("../static/sprites/hud.png", import.meta.url) },
+        { name: "fonts", src: new URL("../static/sprites/fonts.png", import.meta.url) },
+        { name: "characters", src: new URL("../static/sprites/characters.png", import.meta.url) },
+        { name: "projectiles", src: new URL("../static/sprites/projectiles.png", import.meta.url) },
     ];
 
     AssetManager.addAssets(assets);
